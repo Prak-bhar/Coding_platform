dff5903e47c98aeac3010614c261da0b2fcc69d3
Yashdeep <yashdeep677@gmail.com>
2026-04-11 14:24:42 +0530

    Removed old files

--- backend/routes/problems.js
+++
@@ -1,9 +0,0 @@
-// src/routes/problems.js
-import express from 'express';
-import { getProblems } from '../controllers/problemsController.js';
-import { authMiddleware } from '../middleware/auth.js';
-const router = express.Router();
-
-router.get('/', authMiddleware, getProblems);
-
-export default router;
